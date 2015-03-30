744284acee5ac8c7a4f9223067811f89711518f8
QinShuo <qinshuo0370@gmail.com>
2015-03-30 19:23:53 +0800

    re-struct file structure and change CMakeLists

--- MainWindow/MainWindow.h
+++ MainWindow/MainWindow.h
@@ -36,16 +36,9 @@
 #include <vtkImageMapper3D.h>
 #include "vtkMetaImageReader.h"
 #include "vtkDICOMImageReader.h"
-//for visualize
-#include "vtkRenderWindow.h"
-#include "vtkRenderer.h"
-#include "vtkImageViewer2.h"
-#include "vtkImageActor.h"
 
-//vtk reslice
-#include "vtkImageReslice.h"
-#include "vtkWindowLevelLookupTable.h"
-#include "vtkImageMapToColors.h"
+//imageviewer2base class
+#include "ImageViewer2View.h"
 
 // my class
 #include "ResliceView.h"
@@ -136,59 +129,6 @@ private:
 	vtkSmartPointer<vtkImageActor> sagittal_mask_Actor;
 };
 
-
-class vtkimageview2_base : QObject
-{
-	Q_OBJECT
-public:
-	explicit vtkimageview2_base(QWidget *parent = 0);
-	vtkimageview2_base(vtkRenderWindow*,char);
-	~vtkimageview2_base();
-
-	void SetSlice(int x) {this->slice_n = x;};
-	void Set_View_Img(vtkSmartPointer<vtkImageData>);
-	int RenderView(int x);
-	int Slice_Position;
-
-	vtkSmartPointer<vtkRenderer> new_render;
-	vtkSmartPointer<vtkImageViewer2> img_viewer2;
-	vtkSmartPointer<vtkRenderWindow> view_window;
-
-	public slots:
-		void on_scroll_mouse_back(vtkObject*);
-		void on_scroll_mouse_forward(vtkObject*);
-private:
-	double* view_dirX;
-	double* view_dirY;
-	double* view_dirZ;
-
-
-	vtkSmartPointer<vtkImageData> img_to_view;
-	//vtkSmartPointer<vtkImageReslice> reslice;
-	//vtkSmartPointer<vtkWindowLevelLookupTable> lookup_table;
-	//vtkSmartPointer<vtkImageMapToColors> color_map;
-
-	char direction;
-	int  slice_n;
-	int* dimensions;
-	void Set_Direction(char);
-	double* calculate_img_center(vtkSmartPointer<vtkImageData>);
-	void Set_Window(vtkRenderWindow*);
-
-	//qt slot connect
-	vtkSmartPointer<vtkEventQtSlotConnect> m_Connections_mouse_back;
-	vtkSmartPointer<vtkEventQtSlotConnect> m_Connections_mouse_forward;
-};
-
-class interactor_style_viewer2 : public vtkInteractorStyleImage
-{
-public:
-	static interactor_style_viewer2* New();
-	vtkTypeMacro(interactor_style_viewer2, vtkInteractorStyleImage);
-
-	void OnMouseWheelBackward() {};
-	void OnMouseWheelForward() {};
-};
 
 
 
