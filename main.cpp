32b1cef2d24bd6516f29b7e658708390faca20e5
QinShuo <qinshuo0370@gmail.com>
2015-04-12 11:24:31 +0800

    add support to Chinese

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 #include <QApplication>
 #include <QMainWindow>
 
+//
+#include <QTextCodec>
+
+
 int main( int argc, char** argv )
 {
+	QTextCodec *codec=QTextCodec::codecForName("GBK");
+	QTextCodec::setCodecForLocale(codec);   
+	QTextCodec::setCodecForTr(codec);   
+	QTextCodec::setCodecForCStrings(codec);  
+
 	QApplication fmri_main_app(argc,argv);
 
 	MainWindow fmri_main_window;
