7e0bf3f1dd85d40de698d9511bba6274e86667d1
QinShuo <qinshuo0370@gmail.com>
2015-04-20 15:34:24 +0800

    test multi thread

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <QTextCodec>
 
 
+
 int main( int argc, char** argv )
 {
 	QTextCodec *codec=QTextCodec::codecForName("GBK");
@@ -17,21 +18,23 @@ int main( int argc, char** argv )
 
 	QApplication fmri_main_app(argc,argv);
 
-	MainWindow fmri_main_window;
-	ROIBasedPanel roi_based_panel(&fmri_main_window);
+	//MainWindow fmri_main_window;
 
-	fmri_main_window.show();
-	roi_based_panel.show();
+	//ROIBasedPanel roi_based_panel(&fmri_main_window);
+	QWidget	* main_win = new MainWindow;
+	QWidget* bct_panel = new ROIBasedPanel( (MainWindow*) main_win);
+	multi_thread_main* thread_main[1];
+	thread_main[0] = new multi_thread_main(main_win);
+	multi_thread_main* thread_bct[1];
+	thread_bct[0]  = new multi_thread_main(bct_panel);
+
+	thread_main[0]->run();
+	thread_bct[0]->run();
+	//fmri_main_window.show();
+	//roi_based_panel.show();
 
 	return fmri_main_app.exec();
 }
-
-
-
-
-
-
-
 
 
 
